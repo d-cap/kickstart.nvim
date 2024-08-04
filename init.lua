443e7b3aed5daabbb1efa70744a00f8e5d7cc06f
d-cap <davide.capomagi@gmail.com>
2024-08-04 17:15:12 +0100

    feat: add back diagnostic navigation

--- init.lua
+++ init.lua
@@ -178,6 +178,9 @@ vim.opt.isfname:append '@-@'
 vim.opt.colorcolumn = '120'
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier

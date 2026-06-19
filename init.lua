6b8ec4b36fea473a27f112e2f599a93928498fae
d-cap <davide.capomagi@gmail.com>
2026-06-19 06:04:31 +0100

    chore: add lsp languages support

--- init.lua
+++ init.lua
@@ -693,7 +693,7 @@ do
   ---@type table<string, vim.lsp.Config>
   local servers = {
     clangd = {},
-    -- gopls = {},
+    gopls = {},
     -- pyright = {},
     rust_analyzer = {},
     --
@@ -701,7 +701,7 @@ do
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    ts_ls = {},
 
     stylua = {}, -- Used to format Lua code
 
